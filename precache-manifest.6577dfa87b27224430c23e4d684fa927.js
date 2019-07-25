4b1eac5a01116ddd4451c6a49b609bf606d50b04
brandongatlin <brandongatlin1981@me.com>
2019-07-24 20:15:04 -0500

    Updates

---
+++ precache-manifest.6577dfa87b27224430c23e4d684fa927.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae30f67680cceb9e05b9",
+    "url": "/react-chrystal-game/static/css/main.dbc0be62.chunk.css"
+  },
+  {
+    "revision": "ae30f67680cceb9e05b9",
+    "url": "/react-chrystal-game/static/js/main.ae30f676.chunk.js"
+  },
+  {
+    "revision": "4eb854ae48008abcf874",
+    "url": "/react-chrystal-game/static/js/runtime~main.4eb854ae.js"
+  },
+  {
+    "revision": "856281628acf98f4f10c",
+    "url": "/react-chrystal-game/static/js/2.85628162.chunk.js"
+  },
+  {
+    "revision": "e7cf33f4b6917f2d34f60699377b0fcb",
+    "url": "/react-chrystal-game/index.html"
+  }
+];
